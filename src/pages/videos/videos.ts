0a7d8b4627f19df7db2e3b2f888fc0fc73e697fa
Luis Gonzalez <lx.mw001@gmail.com>
2017-03-20 23:51:12 -0500

    Implemented youtube video player

--- src/pages/videos/videos.ts
+++ src/pages/videos/videos.ts
@@ -1,19 +1,28 @@
-import {Component, NgModule} from '@angular/core'
-import {BrowserModule} from '@angular/platform-browser'
+import {Component, NgModule, OnInit} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { YoutubeVideoPlayer } from 'ionic-native';
 
 @Component({
   selector: 'videos-page',
   templateUrl:'videos.html'
 })
-export class VideosPage {
+export class VideosPage implements OnInit{
   title:string;
 
   constructor(){
     this.title="Hello World";
   }
 
+  ngOnInit() {
+  }
+
+  playVideo(videoId) {
+    YoutubeVideoPlayer.openVideo(videoId);
+  }
+
   click(){
     alert('You clicked me !');
   }
+
 
 }
